d5f87ac43110085dc94d48b3579c9951f5ad910c
suapython <morales@roma2.infn.it>
2020-08-01 02:13:39 +0200

    Initial Commit

---
+++ Notes_SwiftUI_Masterminds/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Notes_SwiftUI_Masterminds
+//
+//  Created by jose francisco morales on 01/08/2020.
+//  Copyright © 2020 jose francisco morales. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
